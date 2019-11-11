e432e3b0205ec7a3583cdadfc2f40ce462a8c867
tfranks247 <tfranks247@gmail.com>
2019-11-11 16:27:43 -0500

    changes2

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      ArrayList<User> dbs=new ArrayList<>();
-      User user=new User("Petros","melake");
-        User user1=new User("tony","melake");
-        User user2=new User("monday","melake");
-        User user3=new User("tuesday","melake");
-        User user4=new User("wendsday","melake");
-        User user5=new User("thursday","melake");
+        ArrayList<User> dbs = new ArrayList<>();
+        User user = new User("Petros", "melake");
+        User user1 = new User("tony", "melake");
+        User user2 = new User("monday", "melake");
+        User user3 = new User("tuesday", "melake");
+        User user4 = new User("wendsday", "melake");
+        User user5 = new User("thursday", "melake");
 
-        Collections.addAll(dbs,user,user1,user2,user3,user4,user5);
+        Collections.addAll(dbs, user, user1, user2, user3, user4, user5);
 
         sign(dbs);
 
 
-
-
     }
+
     public static void sign(ArrayList<User> arr) {
         boolean ver = true;
 
@@ -27,27 +26,39 @@ public class Main {
         SignIn singin = new SignIn();
 
 
-            System.out.println("Facebook, Twitter, LinkedIn, or Instagram");
-            System.out.println("Which Social Media do you want to login to: ?");
 
         while (ver) {
-            String ans = key.next();
+
+            System.out.println("Facebook, Twitter, LinkedIn, or Instagram");
+            System.out.println("Which Social Media do you want to login to?");
+
+
+            String ans = key.nextLine();
+
             if (ans.equalsIgnoreCase("facebook")) {
                 singin.signIn(arr, ans);
-                ver=false;
+                ver = false;
             } else if (ans.equalsIgnoreCase("twitter")) {
                 singin.signIn(arr, ans);
-                ver=false;
+                ver = false;
             } else if (ans.equalsIgnoreCase("linkedin")) {
                 singin.signIn(arr, ans);
-                ver=false;
-            } else if (ans.equalsIgnoreCase("Instgram")) {
+                ver = false;
+            } else if (ans.equalsIgnoreCase("Instagram")) {
                 singin.signIn(arr, ans);
-                ver=false;
+                ver = false;
             } else {
                 System.out.println("Please enter one of the above specified platforms");
+                ver = true;
             }
-
+            if (ver == false) {
+                System.out.println("Do you want to login to another account? yes/no");
+                ans = key.nextLine();
+                if (ans.equalsIgnoreCase("yes")) {
+                    ver = true;
+                }
+            }
         }
     }
 }
+
--- src/SignIn.java
+++ src/SignIn.java
@@ -5,21 +5,24 @@ public class SignIn {
     boolean sign = true;
 
     public void signIn(ArrayList<User> arr, String social) {
+        sign = true;
         while (sign) {
             Scanner key = new Scanner(System.in);
             System.out.println("Please enter your email address :");
-            String email = key.next();
+            String email = key.nextLine();
             System.out.println("Please enter new password :");
-            String pass = key.next();
+            String pass = key.nextLine();
             for (User a : arr) {
                 if (a.getName().equalsIgnoreCase(email)) {
                     if (a.getPassword().equalsIgnoreCase(pass)) {
                         System.out.println("you are logged on to " + social);
-                        System.out.println("Do you want log off? (yes/no) :");
-                        String answer = key.next();
+                        System.out.println("Do you want log off? (yes is your only option) :");
+                        String answer = key.nextLine();
                         if(answer.equalsIgnoreCase("yes")) {
                             sign = false;
                             System.out.println("You have logged off");
+
+                            }
                         }
                     }
 
@@ -31,4 +34,3 @@ public class SignIn {
             }
         }
     }
-}
